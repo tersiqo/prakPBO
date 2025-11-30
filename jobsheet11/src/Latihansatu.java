eb171852282c947414b196cec3f0ef7c277a472d
tersiqo <tersiqo.alfarezel@gmail.com>
2025-11-30 14:47:41 +0700

    latian satu

--- jobsheet11/src/Latihansatu.java
+++ jobsheet11/src/Latihansatu.java
@@ -5,10 +5,10 @@ public class Latihansatu {
     public static void main(String[] args) {
 
         // Membuat objek JFrame
-        JFrame frame = new JFrame("Aplikasi Saya");
+        JFrame frame = new JFrame("Latihan Saya");
 
         // Mengatur ukuran jendela
-        frame.setSize(300, 200);
+        frame.setSize(600, 400);
 
         // Menutup aplikasi ketika jendela ditutup
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
